99fdcf47dd67e18eedc3de9cde7b745af7f3fda7
Daniel Shterev <daniel.shterev@sap.com>
2025-11-22 14:54:23 +0200

    added tasks for js advanced 1  nov 23

---
+++ exercises/js-23-Nov-25/task2/script.js
@@ -0,0 +1,20 @@
+const versions = {
+    small: {
+        name: "Продукт S",
+        price: 20,
+        img: "img1.jpg",
+        desc: "<strong>Малка версия</strong> на продукта."
+    },
+    medium: {
+        name: "Продукт M",
+        price: 40,
+        img: "img2.jpg",
+        desc: "<em>Средна версия</em> на продукта."
+    },
+    large: {
+        name: "Продукт L",
+        price: 60,
+        img: "img3.jpg",
+        desc: "<u>Голяма версия</u> на продукта."
+    }
+};
